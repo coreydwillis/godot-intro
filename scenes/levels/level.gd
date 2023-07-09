745633d218d5c12905214a576763e3b62c2faefc
Corey Willis <coreydwillis@gmail.com>
2023-07-08 18:06:11 -0600

    Progress

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,15 +3,8 @@ extends Node2D
 var test_array: Array[String] = ["Test", "Hello", "Stuff"]
 
 func _ready():
-		$Logo.rotation_degrees = 90
-		for i in test_array:
-			print(i)
 
-func _process(delta):
-	
-	$Logo.rotation_degrees += 100 * delta 
+#	print(Input.is_action_pressed("left"))
 
-	if $Logo.position.x > 1000:
-		$Logo.pos.x = 0
-
-	print(Input.is_action_pressed("left"))
+func test_function():
+	print("this is a test function")
---
+++ scenes/player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+func _process(_delta):
+	# input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
+
+	# laser shooting input
+	if Input.is_action_just_pressed("primary action"):
+		print('shoot laser')
+
+	# grenade shooting input
+	if Input.is_action_just_pressed("secondary action"):
+		print('shoot grenade')
